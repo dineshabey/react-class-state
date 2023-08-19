c58d3f3d0447613f7007dce096384f68301516f8
unknown <dinabeysinghe@gmail.com>
2023-08-19 22:15:23 +0530

    add state to constructor

--- src/layouts/HomeLayouts.js
+++ src/layouts/HomeLayouts.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react'
 import NewTaskAddInput from '../components/NewTaskAddInput'
 import TaskList from './homeLayouts/TaskList'
 
-const taskList = ['Task1', 'Task2', 'Task3'];
+// const taskList = ['Task1', 'Task2', 'Task3'];
 
 
 export default class HomeLayouts extends Component {
+    state = {
+        taskListData: [],
+    }
+
+    constructor() {
+        super();
+        this.state = {
+            taskListData: ['Task1', 'Task2', 'Task3']
+        }
+    }
+
     render() {
         return (
             <div style={{ display: 'flex', justifyContent: 'center', margin: '20px' }}>
                 <div>
                     <NewTaskAddInput />
                     <div style={{ marginTop: '50px' }}>
-                        <TaskList taskListData={taskList} />
+                        <TaskList taskListData={this.state.taskListData} />
                     </div>
                 </div>
             </div>
--- src/layouts/homeLayouts/TaskList.js
+++ src/layouts/homeLayouts/TaskList.js
@@ -4,7 +4,6 @@ import SingaleTaskList from '../../components/taskList/SingaleTaskList'
 export default class TaskList extends Component {
     render() {
         const { taskListData } = this.props;
-        console.log(taskListData)
         return (
             <div>
 
