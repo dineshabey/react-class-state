fd99eafb0a1af14f6027c47e45c2b639785a8bcf
unknown <dinabeysinghe@gmail.com>
2023-08-19 21:11:04 +0530

    take onchange value

--- src/components/NewTaskAddInput.js
+++ src/components/NewTaskAddInput.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 export default class NewTaskAddInput extends Component {
+
+  state = {
+    inputValue: '',
+  }
+
+  handleInputValueChange = (e) => {
+    console.log(e.target.value);
+  };
+
   render() {
     return (
       <div>
-        <input type='text' style={{ fontSize: '20px' }}></input>
+        <input onChange={this.handleInputValueChange} type='text' style={{ fontSize: '20px' }}></input>
         <button style={{ fontSize: '20px' }}>Add</button>
       </div>
     )
