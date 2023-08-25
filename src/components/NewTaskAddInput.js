87e700435f617271663c1b0e4dc64a679c0f1053
unknown <dinabeysinghe@gmail.com>
2023-08-25 12:40:13 +0530

    function passs to state

--- src/components/NewTaskAddInput.js
+++ src/components/NewTaskAddInput.js
@@ -7,7 +7,8 @@ export default class NewTaskAddInput extends Component {
   }
 
   handleInputValueChange = (e) => {
-    console.log(e.target.value);
+    // console.log(e.target.value); 
+    this.setState({ inputValue: e.target.value });
   };
 
   render() {
@@ -16,7 +17,7 @@ export default class NewTaskAddInput extends Component {
     return (
       <div>
         <input onChange={this.handleInputValueChange} type='text' style={{ fontSize: '20px' }}></input>
-        <button onClick={addNewTask} style={{ fontSize: '20px' }}>
+        <button onClick={() => addNewTask(this.state.inputValue)} style={{ fontSize: '20px' }}>
           {''}
           Add</button>
       </div>
