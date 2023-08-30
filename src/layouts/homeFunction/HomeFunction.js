2c7b3f24e65fc39beca1671191a81b676c6504f4
unknown <dinabeysinghe@gmail.com>
2023-08-30 20:07:34 +0530

    completed number adding function base componet

--- src/layouts/homeFunction/HomeFunction.js
+++ src/layouts/homeFunction/HomeFunction.js
@@ -3,14 +3,14 @@ import React, { useState } from 'react'
 export default function HomeFunction() {
     const [numberCount, setNumberContent] = useState(1); //function base state variyabale
     const addNumber = () => {
-        
+        setNumberContent(numberCount + 1);
     }
 
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center', margin: '20px' }}>
             <div>
-                <p style={{ fontSize: '40px' }}>{numberCount}</p>
+                <p style={{ fontSize: '40px' , color:'red'}}>{numberCount}</p>
                 <button onClick={addNumber}>Add One</button>
             </div>
         </div>
