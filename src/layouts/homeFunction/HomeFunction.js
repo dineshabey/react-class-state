27e4efa77aa9d73ff6b4c0690d4bc6d056141306
unknown <dinabeysinghe@gmail.com>
2023-08-30 19:59:49 +0530

    create function base componet

--- src/layouts/homeFunction/HomeFunction.js
+++ src/layouts/homeFunction/HomeFunction.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function HomeFunction() {
+    const [numberCount, setNumberContent] = useState(); //function base state variyabale
+    const addNumber = () => {
+        alert()
+    }
+
+
     return (
         <div style={{ display: 'flex', justifyContent: 'center', margin: '20px' }}>
-            HomeFunction</div>
+            <div>
+                <p style={{ fontSize: '40px' }}>1</p>
+                <button onClick={addNumber}>Add One</button>
+            </div>
+        </div>
     )
 }
