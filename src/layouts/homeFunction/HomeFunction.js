238946ad39ed88496f519bd19017aa733c2aa921
unknown <dinabeysinghe@gmail.com>
2023-08-30 20:02:32 +0530

    add function base componet to intial value

--- src/layouts/homeFunction/HomeFunction.js
+++ src/layouts/homeFunction/HomeFunction.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react'
 
 export default function HomeFunction() {
-    const [numberCount, setNumberContent] = useState(); //function base state variyabale
+    const [numberCount, setNumberContent] = useState(1); //function base state variyabale
     const addNumber = () => {
-        alert()
+        
     }
 
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center', margin: '20px' }}>
             <div>
-                <p style={{ fontSize: '40px' }}>1</p>
+                <p style={{ fontSize: '40px' }}>{numberCount}</p>
                 <button onClick={addNumber}>Add One</button>
             </div>
         </div>
