09abe3222f890ed856ca1ec6888619caf16f759f
unknown <dinabeysinghe@gmail.com>
2023-08-30 17:46:35 +0530

    add basic css

--- src/layouts/homeFunction/HomeFunction.js
+++ src/layouts/homeFunction/HomeFunction.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 export default function HomeFunction() {
-  return (
-    <div>HomeFunction</div>
-  )
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', margin: '20px' }}>
+            HomeFunction</div>
+    )
 }
